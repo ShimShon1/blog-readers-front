f7c1a5fdc6cbf7706ceae79fd74bc4b0cc60f364
Dean <deaneffi1414@gmail.com>
2024-05-26 13:25:19 +0300

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# blog readers front
+
+Using my Blog API to let readers view and comment on posts.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="m-20 text-center text-5xl text-emerald-800 ">
+        Welcome to my blog
+      </h1>
+    </>
+  );
+}
+
+export default App;
